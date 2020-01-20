a07b51806f908f7596340b0ff9d5c9d779ab34cb
brandon moro <brandon_078@hotmail.com>
2020-01-20 21:42:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,14 +7,7 @@ actor = {name: "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {
-		:railroads =>
-		{
-			:pieces => 4,
-			:names => {},
-			:rent_in_dollars => {}
-		}
-	}
+	monopoly = {}
 
 end
 
